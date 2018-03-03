6009ecf9e6958b141fc7410047e119cb50f75d17
perceptib1e <perceptib1e@mail.ru>
2018-03-03 04:10:02 +0200

    BIG changes: 1) created buffer Sets between Server and XML files to make workflow more comfortable.  2) creation way of XML files on server was modified to JAXB for making well-formed XML files and for marshalling java objects to XML and unmarshalling backwards from XML to java objects (all methods in PlayerController was remake)  3) added logging out  4) fixed registration

---
+++ server/src/main/java/seaBattle/model/PlayerList.java
@@ -0,0 +1,23 @@
+package seaBattle.model;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.List;
+
+@XmlRootElement(name = "playerList")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class PlayerList
+{
+    @XmlElement(name = "player")
+    private List<Player> playerList = null;
+
+    public List<Player> getPlayerList() {
+        return playerList;
+    }
+
+    public void setPlayerList(List<Player> playerList) {
+        this.playerList = playerList;
+    }
+}
