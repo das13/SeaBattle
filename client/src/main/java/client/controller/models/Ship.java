3b77e789d9079af59f6c8980179df9d964fa47a1
CDP1988 <d02041004>
2018-03-09 06:28:52 +0200

    Added functions: building a list of players, calling for battle, placing ships

---
+++ client/src/main/java/client/controller/models/Ship.java
@@ -0,0 +1,36 @@
+package client.controller.models;
+
+import client.controller.GameController;
+import javafx.scene.paint.Color;
+
+public class Ship {
+
+    //private static int ships[] = new int[]{1,1,1,1,2,2,2,3,3,4};
+    private int x, y, x2, y2, length, position;
+
+
+    public Ship(int x, int y, int length, int position) {
+        this.x = x;
+        this.y = y;
+        this.length = length;
+        this.position = position;
+    }
+
+/*
+    public Ship(int x, int y, int x2, int y2) {
+        this.x = x;
+        this.y = y;
+        this.x2 = x2;
+        this.y2 = y2;
+
+    }
+*/
+
+    public void setShip() {
+        for (int i = 0; i < length; i++) {
+            Cell cell = new Cell(x + i * ((position == 1)?0:1), y + i * ((position == 1)?1:0), false);
+            cell.border.setFill(Color.LIGHTGRAY);
+            GameController.getCwController().getUserPane().getChildren().add(cell);
+        }
+    }
+}
---
+++ client/src/main/java/client/controller/utils/ProgressAnimation.java
@@ -0,0 +1,25 @@
+package client.controller.utils;
+
+import javafx.concurrent.Task;
+
+public class ProgressAnimation extends Task<Integer> {
+
+    @Override
+    protected Integer call() throws Exception {
+        for (int i = 0; i <= 100; i++) {
+            updateProgress(i/100.0 + 0.01, 1.0);
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            if (isCancelled()) {return i;}
+        }
+        return 100;
+    }
+
+    @Override
+    protected void updateProgress(double workDone, double max) {
+        super.updateProgress(workDone, max);
+    }
+}
